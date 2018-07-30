9f639e1b63c15cf15da2c55828dda68db85e6a0a
GrayCatWhiz <official.martgil@gmail.com>
2018-07-29 21:19:43 -0700

    First commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace test_program
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.name = new System.Windows.Forms.TextBox();
+            this.insertBtn = new System.Windows.Forms.Button();
+            this.age = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // name
+            // 
+            this.name.Location = new System.Drawing.Point(47, 36);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(274, 20);
+            this.name.TabIndex = 0;
+            // 
+            // insertBtn
+            // 
+            this.insertBtn.Location = new System.Drawing.Point(47, 194);
+            this.insertBtn.Name = "insertBtn";
+            this.insertBtn.Size = new System.Drawing.Size(263, 23);
+            this.insertBtn.TabIndex = 1;
+            this.insertBtn.Text = "Insert";
+            this.insertBtn.UseVisualStyleBackColor = true;
+            this.insertBtn.Click += new System.EventHandler(this.insertBtn_Click);
+            // 
+            // age
+            // 
+            this.age.Location = new System.Drawing.Point(47, 74);
+            this.age.Name = "age";
+            this.age.Size = new System.Drawing.Size(274, 20);
+            this.age.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 289);
+            this.Controls.Add(this.age);
+            this.Controls.Add(this.insertBtn);
+            this.Controls.Add(this.name);
+            this.Name = "Form1";
+            this.Text = "Dynamic Insert Query Method Example";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox name;
+        private System.Windows.Forms.Button insertBtn;
+        private System.Windows.Forms.TextBox age;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test_program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void insertBtn_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                string sql = "INSERT INTO TESTTABLE(name,age) VALUES(@name,@age)";
+                ExecuteInsert(sql,new string[] {"@name","@age"}, new string[] { name.Text, age.Text });
+                MessageBox.Show("OK");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ExecuteInsert(string query, string[] ParamName, string[] values)
+        {
+            string connStr = "Data Source=" + Environment.MachineName + ";Initial Catalog = testdb;Integrated Security=SSPI; User ID = " + Environment.MachineName + @"\" + Environment.UserName + "; Password =";
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+            SqlParameter[] Param = new SqlParameter[ParamName.Length];
+            SqlCommand cmd = new SqlCommand(query, conn);
+            for (int c = 0; c < ParamName.Length; c++)
+            {
+                Param[c] = new SqlParameter();
+            }
+
+            for (int i = 0; i < ParamName.Length; i++)
+            {
+                Param[i].ParameterName = ParamName[i];
+                Param[i].Value = values[i];
+                cmd.Parameters.Add(Param[i]);
+            }
+            cmd.ExecuteNonQuery();
+
+        }
+    }
+}
